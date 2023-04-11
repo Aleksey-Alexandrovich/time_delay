6dfc02efce2a0a7216d3f8aea7a37893fafc8121
Alexey <84179953+Aleksey-Alexandrovich@users.noreply.github.com>
2023-04-11 17:05:20 +0300

    add new files

--- script.js
+++ script.js
@@ -7,15 +7,23 @@ const paragraph = document.createElement('p');
 document.body.append(input);
 document.body.append(paragraph);
 
+  
+let f = debounce(setParagraph, 300);
+
+function debounce(func, timeout = 300){
+	let timer;
+	return (...args) => {
+	  clearTimeout(timer);
+	  timer = setTimeout(() => { func.apply(this, args); }, timeout);
+	};
+  }
 
 function setParagraph (value) {
 	paragraph.innerHTML = value;
 }
 
 input.addEventListener('input', (e) => {
-	setTimeout(() => {
-		setParagraph(e.target.value);
-	}, 300)
+	f(e.target.value);
 })
 
 
